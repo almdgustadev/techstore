409c5285ad786ffdd43d253e90ca61ec6622b623
almdgustadev <gunna2513@gmail.com>
2025-03-12 10:53:27 -0300

    Add projeto ao repo remoto

---
+++ src/main/java/com/devsalmeida/techstore/entities/Produto.java
@@ -0,0 +1,69 @@
+package com.devsalmeida.techstore.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+    private double preco;
+    private Integer quantidadeEstoque;
+
+    public Produto() {}
+
+    public Produto(Long id, String nome, String descricao, double preco, Integer quantidadeEstoque) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+}
