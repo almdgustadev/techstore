3655a5d37b0679554e7e1ec970f1a2e6788b05ee
almdgustadev <gunna2513@gmail.com>
2025-03-20 23:58:23 -0300

    change package

--- src/main/java/com/devsalmeida/techstore/handler/EmptyStockException.java
+++
@@ -1,9 +0,0 @@
-package com.devsalmeida.techstore.handler;
-
-public class EmptyStockException extends RuntimeException {
-    public EmptyStockException(){super(("Não há produtos em estoque!"));}
-
-    public EmptyStockException(String message) {
-        super(message);
-    }
-}
